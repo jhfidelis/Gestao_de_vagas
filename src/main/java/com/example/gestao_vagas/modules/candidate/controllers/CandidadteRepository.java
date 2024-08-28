1693d239718d04f4e2c43bc68a06b13f5b564877
jhfidelis <fidelis.henrique.jh@gmail.com>
2024-08-27 22:07:19 -0300

    Validando usuário já existente

--- src/main/java/com/example/gestao_vagas/modules/candidate/controllers/CandidadteRepository.java
+++ src/main/java/com/example/gestao_vagas/modules/candidate/controllers/CandidadteRepository.java
@@ -1,5 +1,6 @@
 package com.example.gestao_vagas.modules.candidate.controllers;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.gestao_vagas.modules.candidate.CandidateEntity;
 
 public interface CandidadteRepository extends JpaRepository<CandidateEntity, UUID> {
-    
+    Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
 }
--- src/main/java/com/example/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/example/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.gestao_vagas.exceptions.UserFoundException;
 import com.example.gestao_vagas.modules.candidate.CandidateEntity;
 
 import jakarta.validation.Valid;
@@ -19,6 +20,11 @@ public class CandidateController {
     
     @PostMapping("/")
     public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        this.candidadteRepository
+        .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+        .ifPresent((user) -> {
+            throw new UserFoundException();
+        });
         return this.candidadteRepository.save(candidateEntity);
     }
 
