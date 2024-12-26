40c82d613cef0c830fa0c6e50189a6c57da34e41
jhfidelis <fidelis.henrique.jh@gmail.com>
2024-12-26 18:57:05 -0300

    Criando token JWT de candidato

--- src/main/java/com/example/gestao_vagas/modules/candidate/CandidadteRepository.java
+++ src/main/java/com/example/gestao_vagas/modules/candidate/CandidadteRepository.java
@@ -6,5 +6,9 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CandidadteRepository extends JpaRepository<CandidateEntity, UUID> {
+    
     Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
+    
+    Optional<CandidateEntity> findByUsername(String username);
+
 }
