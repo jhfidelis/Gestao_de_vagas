5f742c71437906fc7a9785b63789d0f550bfa636
jhfidelis <fidelis.henrique.jh@gmail.com>
2024-12-26 00:29:55 -0300

    Criptografando senha do candidato

--- src/main/java/com/example/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/com/example/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package com.example.gestao_vagas.modules.candidate.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.gestao_vagas.exceptions.UserFoundException;
@@ -13,12 +14,19 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidadteRepository candidadteRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidadteRepository
-            .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
-            .ifPresent((user) -> {
-                throw new UserFoundException();
-            });
+                .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
+
         return this.candidadteRepository.save(candidateEntity);
     }
 
